95142d16f8a6d8db658435a46a40451cdf36965a
Mechanic <74297154+TheMechanic57@users.noreply.github.com>
2023-06-12 20:48:54 +0100

    step1

---
+++ CarterInput.py
@@ -0,0 +1,15 @@
+from modules.sanware_carter import *
+from ai_config import *
+
+
+def CarterInput(sentence):
+
+    sentence = input(f"{User}: ")
+    SendToCarter(sentence, User, APIkey)
+
+    with open("CarterResponse.txt") as f:
+        ResponseOutput = f.read()
+
+    print(ResponseOutput)
+
+    speak(ResponseOutput)
