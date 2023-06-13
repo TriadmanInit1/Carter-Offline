2eb7cfaf561f895974d520801afa0af973ed348c
Cipher <74297154+BillCipher57@users.noreply.github.com>
2023-06-13 09:17:20 +0100

    Update ai_config.py

--- ai_config.py
+++ ai_config.py
@@ -7,5 +7,5 @@ AuthorisedUsers = [User]
 
 # Your stuff
 
-APIkey = "28c9e490-3786-4ee3-8f7c-e1e9ff4febcb"
+APIkey = ""
 DiscordAPI = "REPLACE THIS WITH YOUR DISCORD BOT'S API. GET IT FROM DISCORD DEVELOPER WEBSITE."
